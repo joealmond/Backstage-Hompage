24c4b1f153717e709d1993d2e3c9534e96815056
joealmond <jozsef.mandula@gmail.com>
2021-11-13 12:40:25 +0100

    fixed 2nd nav menu

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
-let fn = document.getElementsByClassName("floating-menu")[0];
-fn.style.opacity = "0";
-window.addEventListener("scroll", function () {
-  fn.style.opacity = "0.7";
-  setTimeout(function () {
-    fn.style.opacity = "0";
-  }, 5000);
-});
-document.body.addEventListener(
-  "click",
-  function () {
-    console.log("click");
-    fn.style.opacity = "0.7";
-    setTimeout(function () {
-      fn.style.opacity = "0";
+const toggleMenu = document.querySelectorAll(".collapsible");
+
+const fn = document.getElementsByClassName("floating-menu__menu")[0];
+
+captureClickAndToggle(fn, toggleMenu, "collapsible-expanded");
+
+function captureClickAndToggle(
+  selectedElement,
+  nodeListOfCollapsibleElement,
+  classNameToToggle
+) {
+  selectedElement.addEventListener("click", function () {
+    setTimeout(() => {
+      nodeListOfCollapsibleElement.forEach(function (e) {
+        e.classList.toggle(classNameToToggle);
+      });
     }, 5000);
-  },
-  true
-);
+    nodeListOfCollapsibleElement.forEach(function (e) {
+      e.classList.toggle(classNameToToggle);
+    });
+  });
+}
